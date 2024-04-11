877de5ea4b330c910a8b7b06a77e1e0bbfcb11e2
KimBong0505 <f.jin@aiqveone.co.jp>
2024-04-11 13:15:07 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+  <!-- <h1>Hello Guys!</h1> -->
+   <NuxtPage/>
+  </div>
+</template>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+       <h1>first Page</h1>
+        
+    </div>
+</template>
